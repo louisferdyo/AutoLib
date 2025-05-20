4b86d51185bb8562726bf38a9c1475b43da7f90b
k3rlyn <kerlyndesliaa@gmail.com>
2025-05-20 13:51:37 +0700

    Update page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -170,7 +170,7 @@ export default function RegisterPage() {
                     required
                     value={fullName}
                     onChange={(e) => setFullName(e.target.value)}
-                    className="appearance-none block w-full pl-10 pr-3 py-2 border border-indigo-200 rounded-lg shadow-sm placeholder-indigo-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all duration-300 bg-white/70 backdrop-blur-sm sm:text-sm"
+                    className="appearance-none block w-full pl-10 pr-3 py-2 border border-indigo-200 rounded-lg shadow-sm placeholder-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition-all duration-300 bg-white/70 backdrop-blur-sm sm:text-sm"
                     placeholder="Nama Lengkap Anda"
                   />
                 </div>
